eb5a825c8111d80768dbba392838e97ba8b78214
Prince-Okuru <Okuruprince1@gmail.com>
2022-09-13 07:38:59 -0700

    a program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
- * main - fuction
- * 
- * Return: Always 0.
+ * main - print putchar
+ * Return: always 0
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -14,9 +15,8 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
+
 	return (0);
-}
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
+
 }
