60f1f0e226af75e324834107f8c4286a5b5738a7
Prince-Okuru <Okuruprince1@gmail.com>
2022-09-22 04:11:57 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *_strncat- append string
+ *@dest: checked
+ *@src: checked
+ *@n: checked
+ *Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+		j++;
+	}
+
+	i = 0;
+
+	while (*(src + i) != *(src + n))
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+
+	return (dest);
+}
