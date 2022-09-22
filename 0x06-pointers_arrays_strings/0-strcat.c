d3cbf037d18238fcce42ef06d588724c1f10306c
Prince-Okuru <Okuruprince1@gmail.com>
2022-09-22 03:59:28 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *_strcat- append string
+ *@dest: checked
+ *@src: checked
+ *Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+
+	j = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+		j++;
+	}
+
+	i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+
+	return (dest);
+}
