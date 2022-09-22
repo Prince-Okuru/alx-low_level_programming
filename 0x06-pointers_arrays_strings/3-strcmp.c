fbcdb7af578e3dbc875eb3b60ae1326286a48e80
Prince-Okuru <Okuruprince1@gmail.com>
2022-09-22 09:50:11 -0700

    Some of my files

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_strcmp- copies the string pointed to by src,
+ *
+ *@s1: checked
+ *@s2:checked
+ *Return: int
+ */
+
+
+int _strcmp(char *s1, char  *s2)
+{
+	int i, output;
+
+	i = 0;
+
+	while (*(s1 + i) == *(s2 + i) && *(s2 + i) && *(s1 + i) != '\0')
+		i++;
+	output = (*(s1 + i) - *(s2 + i));
+	return (output);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *cap_string- converts small letters to cap
+ *@s: checked
+ *Return: s
+ */
+char *cap_string(char *s)
+{
+	int i, j, separatorFound;
+
+	separatorFound = 1;
+
+	for (i = 0; s[i]; ++i)
+	{
+		if (separatorFound && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] -  32;
+		}
+		separatorFound = 0;
+		for (j = 0; j < 12; j++)
+		{
+			if (s[i] == '\t' || s[i] == '\n' || s[i] == ','
+			    || s[i] == '\"' || s[i] == '.' || s[i] == '!' ||
+			    s[i] == '{' || s[i] == '}' || s[i] == '(' ||
+			    s[i] == ')' || s[i] == ' ' || s[i] == '?')
+			{
+				separatorFound = 1;
+			}
+		}
+	}
+	return (s);
+}
